82ea5b6334313810e1d247f7b4ce32fe8dda8643
acurt7 <curtright.a@husky.neu.edu>
2019-10-22 13:07:03 -0700

    Implemented Suggestions Controller

--- src/main/java/io/swagger/api/SuggestionsApiController.java
+++ src/main/java/io/swagger/api/SuggestionsApiController.java
@@ -1,8 +1,10 @@
 package io.swagger.api;
 
+import io.swagger.model.PizzaSize;
 import io.swagger.model.PizzaSuggestion;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
+import org.omg.CORBA.INTERNAL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -31,24 +33,119 @@ public class SuggestionsApiController implements SuggestionsApi {
 
     private final HttpServletRequest request;
 
+    private static final int SLICES_PER_ADULT = 3;
+
+    private static final int SLICES_PER_CHILD = 1;
+
+    private static final int SLICES_PER_SMALL = 6;
+
+    private static final int SLICES_PER_MEDIUM = 8;
+
+    private static final int SLICES_PER_LARGE= 10;
+
     @org.springframework.beans.factory.annotation.Autowired
     public SuggestionsApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
     }
 
-    public ResponseEntity<PizzaSuggestion> getNumberOfPizzas(@NotNull @ApiParam(value = "Number of adults", required = true) @Valid @RequestParam(value = "adults", required = true) Integer adults,@NotNull @ApiParam(value = "Number of children", required = true) @Valid @RequestParam(value = "children", required = true) Integer children,@ApiParam(value = "The preferred size, if given all suggested pizzas will be this size. Must be a valid size (small, medium, large).") @Valid @RequestParam(value = "preferredSize", required = false) String preferredSize) {
+    public ResponseEntity<PizzaSuggestion> getNumberOfPizzas(
+        @NotNull @ApiParam(value = "Number of adults", required = true)
+        @Valid @RequestParam(value = "adults", required = true) Integer adults,
+        @NotNull @ApiParam(value = "Number of children", required = true)
+        @Valid @RequestParam(value = "children", required = true) Integer children,
+        @ApiParam(value = "The preferred size, if given all suggested pizzas will be this size. Must be a valid size (small, medium, large).")
+        @Valid @RequestParam(value = "preferredSize", required = false) String preferredSize) {
+
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
-                return new ResponseEntity<PizzaSuggestion>(objectMapper.readValue("{\n  \"small\" : 2,\n  \"large\" : 0,\n  \"medium\" : 3\n}", PizzaSuggestion.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
+                PizzaSuggestion suggestion;
+                if (preferredSize == null) {
+                    //request paramater is not specified
+                    suggestion = getSuggestion(adults, children);
+                } else {
+                    PizzaSize prefSizeEnum = PizzaSize.fromValue(preferredSize);
+                    if (prefSizeEnum == null) {
+                        return new ResponseEntity<PizzaSuggestion>(HttpStatus.BAD_REQUEST);
+                    }
+                    suggestion = getSuggestion(prefSizeEnum, adults, children);
+                }
+                return new ResponseEntity<PizzaSuggestion>(suggestion, HttpStatus.OK);
+            } catch (Exception e) {
+                log.error("Internal Server Error", e);
                 return new ResponseEntity<PizzaSuggestion>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
-
         return new ResponseEntity<PizzaSuggestion>(HttpStatus.NOT_IMPLEMENTED);
     }
+
+    /**
+     *  Creates a PiazzaSuggestion with no preferred size. Priortizes larger pizzas first
+     * @param adults the number of adults
+     * @param children the number of children
+     * @return
+     */
+    private PizzaSuggestion getSuggestion(Integer adults, Integer children) {
+        Integer totalSlices = (adults * SLICES_PER_ADULT) + (children * SLICES_PER_CHILD);
+        int small = 0;
+        int medium = 0;
+        int large = 0;
+        large = totalSlices / SLICES_PER_LARGE;
+        int remainder = totalSlices % SLICES_PER_LARGE;
+        medium = remainder / SLICES_PER_MEDIUM;
+        remainder = remainder % SLICES_PER_MEDIUM;
+        small = remainder / SLICES_PER_SMALL;
+        remainder = remainder % SLICES_PER_SMALL;
+        if (remainder > 0) {
+            small++;
+        }
+        PizzaSuggestion suggestion = new PizzaSuggestion();
+        suggestion.setSmall(small);
+        suggestion.setMedium(medium);
+        suggestion.setLarge(large);
+        return suggestion;
+    }
+
+    /**
+     * Gets a suggestion for the number of pizzas for a group. This returns all pizzas of the size
+     * get given by preferredSize.
+     * @param preferredSize The size all pizzas in the suggestion should be
+     * @param adults The number of adults
+     * @param children The numbe of childre
+     * @return A pizza suggestion object containing the order reccomendation
+     */
+    private PizzaSuggestion getSuggestion(PizzaSize preferredSize, Integer adults, Integer children) {
+        Integer totalSlices = (adults * SLICES_PER_ADULT) + (children * SLICES_PER_CHILD);
+        int small = 0;
+        int medium = 0;
+        int large = 0;
+        switch(preferredSize) {
+            case SMALL:
+                small = totalSlices / SLICES_PER_SMALL;
+                if ( totalSlices % SLICES_PER_SMALL != 0) {
+                    small++;
+                }
+                break;
+            case MEDIUM:
+                medium = totalSlices / SLICES_PER_MEDIUM;
+                if ( totalSlices % SLICES_PER_MEDIUM != 0) {
+                    medium++;
+                }
+                break;
+            case LARGE:
+                large = totalSlices / SLICES_PER_LARGE;
+                if ( totalSlices % SLICES_PER_LARGE != 0) {
+                    large++;
+                }
+                break;
+        }
+        PizzaSuggestion suggestion = new PizzaSuggestion();
+        suggestion.setSmall(small);
+        suggestion.setMedium(medium);
+        suggestion.setLarge(large);
+        return suggestion;
+    }
+
 
 }
--- src/test/java/io/swagger/api/SuggestionsApiControllerIntegrationTest.java
+++ src/test/java/io/swagger/api/SuggestionsApiControllerIntegrationTest.java
@@ -1,15 +1,18 @@
 package io.swagger.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.model.PizzaSuggestion;
 
 import java.util.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
@@ -21,13 +24,48 @@ public class SuggestionsApiControllerIntegrationTest {
     @Autowired
     private SuggestionsApi api;
 
+    private ObjectMapper objectMapper;
+    @Before
+    public void setUp() {
+        objectMapper = new ObjectMapper();
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Accept", "application/json");
+        api = new SuggestionsApiController(objectMapper, request);
+    }
+
+
     @Test
-    public void getNumberOfPizzasTest() throws Exception {
-        Integer adults = 56;
-        Integer children = 56;
-        String preferredSize = "preferredSize_example";
+    public void getNumberOfPizzasTestSimpleLargeSize() throws Exception {
+        Integer adults = 3;
+        Integer children = 1;
+        String preferredSize = "large";
         ResponseEntity<PizzaSuggestion> responseEntity = api.getNumberOfPizzas(adults, children, preferredSize);
-        assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals((Integer)1, responseEntity.getBody().getLarge());
+        assertEquals((Integer)0, responseEntity.getBody().getMedium());
+        assertEquals((Integer)0, responseEntity.getBody().getSmall());
+    }
+
+    @Test
+    public void getNumberOfPizzasTestSimpleNoPreference() throws Exception {
+        Integer adults = 3;
+        Integer children = 1;
+        ResponseEntity<PizzaSuggestion> responseEntity = api.getNumberOfPizzas(adults, children, null);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals((Integer)1, responseEntity.getBody().getLarge());
+        assertEquals((Integer)0, responseEntity.getBody().getMedium());
+        assertEquals((Integer)0, responseEntity.getBody().getSmall());
+    }
+
+    @Test
+    public void getNumberOfPizzasTestNoPreferenceMultipleTypes() throws Exception {
+        Integer adults = 6;
+        Integer children = 1;
+        ResponseEntity<PizzaSuggestion> responseEntity = api.getNumberOfPizzas(adults, children, null);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals((Integer)1, responseEntity.getBody().getLarge());
+        assertEquals((Integer)1, responseEntity.getBody().getMedium());
+        assertEquals((Integer)1, responseEntity.getBody().getSmall());
     }
 
 }
