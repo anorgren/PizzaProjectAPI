d2b5d8194bad80d5588f11722886fb3410104f10
Adamm Norgren <norgren.a@husky.neu.edu>
2019-11-13 18:53:43 -0800

    added pizza repo

---
+++ src/main/java/io/swagger/repository/PizzaRepository.java
@@ -0,0 +1,8 @@
+package io.swagger.repository;
+
+import io.swagger.model.Pizza;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PizzaRepository extends MongoRepository<Pizza, String> {
+    Pizza getPizzaByPizzaName(String pizzaName);
+}
