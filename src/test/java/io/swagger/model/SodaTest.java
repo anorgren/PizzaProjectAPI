ff6e0bc4507f1fe2bd50b757bbfd18593244004e
Adamm Norgren <norgren.a@husky.neu.edu>
2019-11-15 10:26:56 -0800

    added simple tests for soda models price

---
+++ src/test/java/io/swagger/model/SodaTest.java
@@ -0,0 +1,30 @@
+package io.swagger.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SodaTest {
+
+    @Test
+    public void testSixPackPrice() {
+        Soda soda = new Soda();
+        soda.size(Soda.SizeEnum.SIX_PACK);
+        assertEquals(3.49d, soda.getPrice(), 0.0);
+    }
+
+    @Test
+    public void testTwoLiterPrice() {
+        Soda soda = new Soda();
+        soda.size(Soda.SizeEnum.TWO_LITER);
+        assertEquals(2.59d, soda.getPrice(), 0.0);
+    }
+
+
+    @Test
+    public void testTwentyOzPrice() {
+        Soda soda = new Soda();
+        soda.size(Soda.SizeEnum.TWENTY_OUNCE_BOTTLE);
+        assertEquals(1.75d, soda.getPrice(), 0.0);
+    }
+}
