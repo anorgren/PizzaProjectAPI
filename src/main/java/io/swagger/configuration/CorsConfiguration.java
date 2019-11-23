edd142a579b77605ac3099a10bfa54517604d101
Adamm Norgren <norgren.a@husky.neu.edu>
2019-11-23 00:26:48 -0800

    fixed config to implement adapter

--- src/main/java/io/swagger/configuration/CorsConfiguration.java
+++ src/main/java/io/swagger/configuration/CorsConfiguration.java
@@ -1,21 +1,17 @@
 package io.swagger.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfiguration {
+@EnableWebMvc
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*")
-                        .allowedMethods("GET", "PUT");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "PUT");
     }
 }
