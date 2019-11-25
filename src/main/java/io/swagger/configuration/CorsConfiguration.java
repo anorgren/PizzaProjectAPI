832ce1800dadf284223d02bc23142b17da263ed4
Adamm Norgren <norgren.a@husky.neu.edu>
2019-11-24 23:32:05 -0800

    working API with cors enabled

--- src/main/java/io/swagger/configuration/CorsConfiguration.java
+++ src/main/java/io/swagger/configuration/CorsConfiguration.java
@@ -1,17 +1,30 @@
 package io.swagger.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfiguration extends WebMvcConfigurerAdapter {
+public class CorsConfiguration {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
-                .allowedMethods("GET", "PUT");
+    public CorsConfiguration() {
+        super();
+    }
+
+    /**
+     * Bean to define global CORS.
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                        .allowedOrigins("*");
+            }
+        };
     }
 }
