0f51cc0de2e6100f3a83168a70a38157ad32c34a
Adamm Norgren <norgren.a@husky.neu.edu>
2019-11-27 18:11:26 -0800

    cors changes

--- src/main/java/io/swagger/configuration/CorsConfiguration.java
+++ src/main/java/io/swagger/configuration/CorsConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
+/**
+ * Class structure/code taken from:
+ * https://stackoverflow.com/questions/51720552/enabling-cors-globally-in-spring-boot
+ */
 @Configuration
 public class CorsConfiguration {
 
